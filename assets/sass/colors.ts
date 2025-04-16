9a1294557b5907c7196c8ed4b304b1e6f7804349
Manjuraju <manjurajumr5@gmail.com>
2025-04-16 16:28:58 +0530

    Auto-generated commit

---
+++ assets/sass/colors.ts
@@ -0,0 +1,11 @@
+export class Colors {
+    
+   public static Text_Primary = '#000000';
+   public static  Text_secondary = '#ffffff';
+   public static  Page_Background = '#ffffff';
+   public static  Page_Background1 = '#FAFAFA';
+   public static Theme_Color = '#FAD05B';
+   public static Transparent1 = `rgba(255, 255, 255, 0.1)`;
+   public static Transparent2 = `rgba(255, 255, 255, 0.2)`;
+
+}
